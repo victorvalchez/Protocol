048afe9b7b0ee6ed3c3d111f1957995b7eea00c5
Victor Valencia <victorvalchez@gmail.com>
2026-01-09 11:34:53 +0100

    added style guide for colors

---
+++ Protocol/StyleGuide.swift
@@ -0,0 +1,31 @@
+//
+//  StyleGuide.swift
+//  Protocol
+//
+//  Created by Víctor Valencia on 9/1/26.
+//
+
+import SwiftUI
+
+extension Color {
+    static let protocolBackground = Color(hex: "F5F5F7")
+    static let protocolAccent = Color(hex: "D0FF00")
+    static let protocolText = Color(hex: "121212")
+    static let protocolSecondary = Color(hex: "121212").opacity(0.6)
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default: (a, r, g, b) = (1, 1, 1, 0)
+        }
+        self.init(.sRGB, red: Double(r)/255, green: Double(g)/255, blue: Double(b)/255, opacity: Double(a)/255)
+    }
+}
